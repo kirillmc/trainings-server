763b397cb0a584f5ec0de9a84c892e7fa5a3ba54
kirillmc <kirillmcdev@gmail.com>
2024-04-14 14:18:10 +0300

    repo and db created

---
+++ Makefile
@@ -0,0 +1,51 @@
+include .env
+
+LOCAL_BIN:=$(CURDIR)/bin
+
+LOCAL_MIGRATION_DIR=$(MIGRATION_DIR)
+LOCAL_MIGRATION_DSN="host=localhost port=$(PG_PORT) dbname=$(PG_DATABASE_NAME) user=$(PG_USER) password=$(PG_PASSWORD) sslmode=disable"
+
+install-golangci-lint:
+	GOBIN=$(LOCAL_BIN) go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.53.3
+
+lint:
+	bin/golangci-lint run ./... --config .golangci.pipeline.yaml
+
+
+install-deps:
+	GOBIN=$(LOCAL_BIN) go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28.1
+	GOBIN=$(LOCAL_BIN) go install -mod=mod google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
+	GOBIN=$(LOCAL_BIN) go install github.com/pressly/goose/v3/cmd/goose@v3.14.0
+	GOBIN=$(LOCAL_BIN) go install github.com/envoyproxy/protoc-gen-validate@v0.10.1
+	GOBIN=$(LOCAL_BIN) go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@v2.15.2
+	GOBIN=$(LOCAL_BIN) go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@v2.15.2
+	GOBIN=$(LOCAL_BIN) go install github.com/rakyll/statik@v0.1.7
+
+get-deps:
+	go get -u google.golang.org/protobuf/cmd/protoc-gen-go
+	go get -u google.golang.org/grpc/cmd/protoc-gen-go-grpc
+
+generate:
+
+generate-training-api:
+	mkdir -p pkg/training_v1
+	protoc --proto_path api/training_v1  \
+	--go_out=pkg/training_v1 --go_opt=paths=source_relative \
+	--plugin=protoc-gen-go=$(LOCAL_BIN)/protoc-gen-go \
+	--go-grpc_out=pkg/training_v1 --go-grpc_opt=paths=source_relative \
+	--plugin=protoc-gen-go-grpc=$(LOCAL_BIN)/protoc-gen-go-grpc \
+	--validate_out lang=go:pkg/training_v1 --validate_opt=paths=source_relative \
+    --plugin=protoc-gen-validate=$(LOCAL_BIN)/protoc-gen-validate \
+	api/training_v1/training.proto
+
+local-migration-status:
+	$(LOCAL_BIN)/goose -dir ${LOCAL_MIGRATION_DIR} postgres ${LOCAL_MIGRATION_DSN} status -v
+
+create-migration:
+	$(LOCAL_BIN)/goose -dir ${LOCAL_MIGRATION_DIR}  create trainigs_tables sql
+
+local-migration-up:
+	$(LOCAL_BIN)/goose -dir ${LOCAL_MIGRATION_DIR} postgres ${LOCAL_MIGRATION_DSN} up -v
+
+local-migration-down:
+	$(LOCAL_BIN)/goose -dir ${LOCAL_MIGRATION_DIR} postgres ${LOCAL_MIGRATION_DSN} down -v
---
+++ migrations/20240413210123_trainigs_tables.sql
@@ -0,0 +1,97 @@
+-- +goose Up
+create table train_programs
+(
+    id         serial primary key,
+    program_name   varchar(50)      not null,
+    description      varchar(500)      not null,
+    is_public boolean not null default false
+);
+
+
+
+create table train_days
+(
+    id         serial primary key,
+    day_name   varchar(50)      not null,
+    description      varchar(500)      not null
+);
+
+
+
+create table exercises
+(
+    id         serial primary key,
+    exercise_name   varchar(50)      not null,
+    pictures      varchar(500)      not null,
+    description      varchar(500)      not null
+);
+
+
+
+create table sets
+(
+    id         serial primary key,
+    quantity   int      not null,
+    weight      double precision      not null
+);
+
+
+
+create table statistics
+(
+    id         serial primary key,
+    date timestamp not null default now(),
+    quantity   int      not null,
+    weight      double precision      not null,
+    set_id int not null,
+    exercise_id int not null,
+    trains_day_id int not null,
+    program_id int not null
+);
+
+create table days_programs
+(
+    id         serial primary key,
+    program_id   int      not null,
+    day_id      int      not null,
+    foreign key (program_id) references train_programs(id) on delete cascade,
+    foreign key (day_id) references train_days(id) on delete cascade
+);
+create table exercises_days
+(
+    id         serial primary key,
+    day_id   int      not null,
+    exercise_id      int      not null,
+    foreign key (day_id) references train_days(id) on delete cascade,
+    foreign key (exercise_id) references exercises(id) on delete cascade
+);
+
+create table sets_exercises
+(
+    id         serial primary key,
+    exercise_id      int      not null,
+    set_id   int      not null,
+    foreign key (exercise_id) references exercises(id) on delete cascade,
+    foreign key (set_id) references sets(id) on delete cascade
+);
+
+create table statistics_sets
+(
+    id         serial primary key,
+    set_id   int      not null,
+    statistic_id      int      not null,
+    foreign key (set_id) references sets(id) on delete cascade,
+    foreign key (statistic_id) references statistics(id) on delete cascade
+);
+
+
+-- +goose Down
+drop table statistics_sets;
+drop table sets_exercises;
+drop table exercises_days;
+drop table days_programs;
+drop table statistics;
+drop table sets;
+drop table exercises;
+drop table train_days;
+drop table train_programs;
